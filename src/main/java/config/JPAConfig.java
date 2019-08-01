4c64690ed527bab872e1e24615c8ce8741520b51
MyGit49 <2285246577@qq.com>
2019-08-01 16:31:07 +0800

    个人博客展示系统相关配置

---
+++ src/main/java/config/JPAConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * JPA配置
+ */
+@Configuration//配置类注解
+@EntityScan("com.zjw.blog.entity")//实体类扫描路径
+@EnableJpaRepositories("com.zjw.blog.repository")//接口仓库地址
+public class JPAConfig {
+}
