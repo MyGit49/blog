78d327b843178ed99853a9b304991ab50ccddfb6
MyGit49 <2285246577@qq.com>
2019-12-24 15:11:54 +0800

    开发中常用工具类_Excel导出

--- src/main/java/com/zjw/blog/controller/TestController.java
+++ src/main/java/com/zjw/blog/controller/TestController.java
@@ -3,13 +3,17 @@ package com.zjw.blog.controller;
 import com.zjw.blog.entity.User;
 import com.zjw.blog.repository.UserRepository;
 import com.zjw.blog.service.UserService;
+import com.zjw.utils.ExcelUtil.ExcelUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/Test")
 public class TestController {
 
     @Autowired
@@ -40,5 +44,41 @@ public class TestController {
             e.printStackTrace();
             return "error";
         }
+    }
+
+    /**
+     * Excel表格导出接口
+     * http://localhost:15000/blog/Test/ExcelDownload
+     * @param response response对象
+     * @throws IOException 抛IO异常
+     */
+    @RequestMapping("/ExcelDownload")
+    public void excelDownload(HttpServletResponse response) throws IOException {
+
+        List<List<String>> excelData = new ArrayList<>();
+
+        List<String> head = new ArrayList<>();
+        head.add("第一列");
+        head.add("第二列");
+        head.add("第三列");
+
+        List<String> data1 = new ArrayList<>();
+        data1.add("123");
+        data1.add("234");
+        data1.add("345");
+
+        List<String> data2 = new ArrayList<>();
+        data2.add("abc");
+        data2.add("bcd");
+        data2.add("cde");
+
+        excelData.add(head);
+        excelData.add(data1);
+        excelData.add(data2);
+
+        String sheetName = "测试";
+        String fileName = "ExcelTest.xls";
+
+        ExcelUtil.exportExcel(response, excelData, sheetName, fileName, 15);
     }
 }
